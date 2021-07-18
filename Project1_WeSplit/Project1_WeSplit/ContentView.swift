80a58b64357bf9fbe80a9e04b156b1fe5d26846d
Alok <alokme123@gmail.com>
2021-07-18 15:59:49 +0530

    doc: add comments

--- Project1_WeSplit/Project1_WeSplit/ContentView.swift
+++ Project1_WeSplit/Project1_WeSplit/ContentView.swift
@@ -9,25 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //MARK: VARIABLES
     @State private var amount = ""
-    @State private var numberPeople = "2"
+    @State private var numberPeople = ""
     @State private var tipPercentage = 2
     let tips = [2,5,10,15,20]
     
+    //TOTAL CALCUATION
     var total: Double {
         let tipP = Double(tips[tipPercentage])
         let dAmount = Double(amount) ?? 0
         let finalAmt = Double((tipP/100.0)*dAmount + dAmount)
         return finalAmt
     }
-    
+    //CONVERTING TO PER PERSON
     var perPerson: Double {
         let peopleCount = Double(numberPeople) ?? 0 + 2
         var finalAmt = total
         finalAmt = finalAmt/peopleCount
         return finalAmt
     }
-    
+    //MARK: BODY
     var body: some View {
         NavigationView{
             Form{
@@ -35,29 +37,27 @@ struct ContentView: View {
                     TextField("Amount", text: $amount)
                     .keyboardType(.decimalPad)
                     Picker("Number of People", selection: $numberPeople){
-//                        ForEach(2 ..< 101){
-//                            Text("\($0)")
-//                        }
                         TextField("Number of People", text: $numberPeople)
                             .keyboardType(.numberPad)
-                    }
-                }
+                    }//PICKER
+                }//INPUT SECTION 1
                 Section(header: Text("How much do you wanna tip?")){
                     Picker("Tip Percentage", selection: $tipPercentage){
                         ForEach(0 ..< tips.count){
                             Text("\(self.tips[$0])%")
-                        }
-                    }.pickerStyle(SegmentedPickerStyle())
-                }
+                        }//FOR
+                    }//PICKER 2
+                    .pickerStyle(SegmentedPickerStyle())
+                }//INPUT SECTION 2
                 Section(header: Text("Total Amount")){
                     Text("Amount: ₹ \(total, specifier:"%0.2f")")
-                }
+                }//OUTPUT SECTION 3
                 Section(header: Text("Amount per Person")){
                     Text("Amount: ₹ \(perPerson, specifier:"%0.2f")")
-                }
-            }
+                }//OUTPUT SECTION 4
+            }//FORM
         .navigationBarTitle("WeSplit")
-        }
+        }//NAVIGATION VIEW
     }
 }
 
